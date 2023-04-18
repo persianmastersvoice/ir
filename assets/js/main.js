7ba4a2259a6c255decb5f9918b3408775c8ada8f
Persian masters voice <120068514+persianmastersvoice@users.noreply.github.com>
2023-04-18 01:14:06 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,4 +116,12 @@
   dotImage.addEventListener("click", () => {
     dropdownMenu.style.display = dropdownMenu.style.display === "none" ? "block" : "none";
   });
+
+    // Dropdown menu2
+  const dotImage = document.getElementById("dot-image");
+  const dropdownMenu2 = document.getElementById("dropdown-menu2");
+
+  dotImage.addEventListener("click", () => {
+    dropdownMenu2.style.display = dropdownMenu2.style.display === "none" ? "block" : "none";
+  });
 })();
